a77fa46c533252a9a86e6c933b4e5bbbb781a8e2
sagar27012001 <sagarpatil27012001@gmail.com>
2022-01-20 15:19:13 +0530

    commit 2

--- src/main/java/com/example/MongoDBRestAPI/SequenceGeneratorService.java
+++ src/main/java/com/example/MongoDBRestAPI/SequenceGeneratorService.java
@@ -4,7 +4,6 @@ import static org.springframework.data.mongodb.core.query.Query.query;
 import java.util.Objects;
 import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
