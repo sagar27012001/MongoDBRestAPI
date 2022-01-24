857469db07b79cf85386c036b7f9eb911aa6edda
sagar27012001 <sagarpatil27012001@gmail.com>
2022-01-24 13:42:59 +0530

    Exception Handling 2

--- src/main/java/com/example/MongoDBRestAPI/controller/StudentController.java
+++ src/main/java/com/example/MongoDBRestAPI/controller/StudentController.java
@@ -1,7 +1,6 @@
 package com.example.MongoDBRestAPI.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mapping.MappingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
